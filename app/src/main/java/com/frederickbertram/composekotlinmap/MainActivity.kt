7001ef00d1ec4f49d89ae2fb18bea87d50f04443
frederick bertram <fred.bertram@gmail.com>
2021-10-18 14:59:40 +0800

    added maps requirements aswell as definition of inital row

--- app/src/main/java/com/frederickbertram/composekotlinmap/MainActivity.kt
+++ app/src/main/java/com/frederickbertram/composekotlinmap/MainActivity.kt
@@ -3,11 +3,15 @@ package com.frederickbertram.composekotlinmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.frederickbertram.composekotlinmap.ui.theme.ComposeKotlinMapTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +21,12 @@ class MainActivity : ComponentActivity() {
             ComposeKotlinMapTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Row(modifier = Modifier.height( 40.dp)) {
+
+                        Header("Kotlin Map")
+
+                    }
+
                 }
             }
         }
@@ -25,7 +34,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Header(name: String) {
     Text(text = "Hello $name!")
 }
 
@@ -33,6 +42,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeKotlinMapTheme {
-        Greeting("Android")
+        Header("Android")
     }
 }
