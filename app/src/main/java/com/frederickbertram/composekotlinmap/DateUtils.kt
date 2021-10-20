3d129755a2bd1cb3f0b7d34a77941474405c6385
frederick bertram <fred.bertram@gmail.com>
2021-10-20 12:05:04 +0800

    Date logic

---
+++ app/src/main/java/com/frederickbertram/composekotlinmap/DateUtils.kt
@@ -0,0 +1,14 @@
+package com.frederickbertram.composekotlinmap
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+    val formatter = SimpleDateFormat(format, locale)
+    return formatter.format(this)
+}
+
+    fun getCurrentDateTime(): Date {
+        return Calendar.getInstance().time
+    }
