0ae4b9fb3c3c3f85249172be66198996ddeb1cdd
frederick bertram <fred.bertram@gmail.com>
2021-10-20 13:04:28 +0800

    Added a model and httpOK3

--- app/src/main/java/com/frederickbertram/composekotlinmap/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/frederickbertram/composekotlinmap/viewmodel/MainViewModel.kt
@@ -1,7 +1,11 @@
 package com.frederickbertram.composekotlinmap.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.frederickbertram.composekotlinmap.view.MapView
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 
 class MainViewModel() : ViewModel() {
@@ -11,7 +15,31 @@ class MainViewModel() : ViewModel() {
 
     init{
         //fetch json data
+        fetchJson();
 
+    }
+
+    fun fetchJson() {
+        val url="https://github.com/elementengineering/Mobile-App-Coding-Challenge/blob/master/data/data.json"
+        val request= Request.Builder().url(url).build()
+        val client= OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+                val body= response.body?.
+
+
+
+                val gson= GsonBuilder().create()
+                //Log.d("JSONDATA",gson.fromJson(body))
+
+
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+        })
     }
 
 
