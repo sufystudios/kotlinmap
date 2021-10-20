a8d91cca411b8dbb7ecacc852f87b68bba6b1c2d
frederick bertram <fred.bertram@gmail.com>
2021-10-20 11:55:30 +0800

    time logic to be implemented later

--- app/src/main/java/com/frederickbertram/composekotlinmap/MainActivity.kt
+++ app/src/main/java/com/frederickbertram/composekotlinmap/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.frederickbertram.composekotlinmap
 
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.d
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -13,20 +15,27 @@ import com.frederickbertram.composekotlinmap.ui.theme.ComposeKotlinMapTheme
 import com.frederickbertram.composekotlinmap.view.MapView
 import com.frederickbertram.composekotlinmap.viewmodel.MainViewModel
 import rememberMapViewWithLifecycle
+import java.text.SimpleDateFormat
 
 class MainActivity : ComponentActivity() {
 
     val mainViewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+val time = java.util.Calendar.getInstance().time
+
+
+        val sdf = SimpleDateFormat("hh:mm:ss")
+
+        Log.d(" C DATE is  ", sdf.format(time))
         setContent {
             ComposeKotlinMapTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
 
-                        //Header("Kotlin Map")
+                        Header("Kotlin Map - time is - " + sdf.format(time))
 
-                        MapView(mainViewModel = mainViewModel)
+                        //MapView(mainViewModel = mainViewModel)
 
                 }
             }
