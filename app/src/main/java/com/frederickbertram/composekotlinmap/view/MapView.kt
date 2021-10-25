5429dc0888088f34bacb4a51d1af14d7ec14ef54
frederick bertram <fred.bertram@gmail.com>
2021-10-25 17:45:04 +0800

    Tinkered with the map furtherto get it to be inline with task

--- app/src/main/java/com/frederickbertram/composekotlinmap/view/MapView.kt
+++ app/src/main/java/com/frederickbertram/composekotlinmap/view/MapView.kt
@@ -1,5 +1,6 @@
 package com.frederickbertram.composekotlinmap.view
 
+import android.util.Half.toFloat
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -10,6 +11,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.viewinterop.AndroidView
 import com.frederickbertram.composekotlinmap.viewmodel.MainViewModel
 import com.google.android.libraries.maps.CameraUpdate
+import com.google.android.libraries.maps.CameraUpdateFactory
+import com.google.android.libraries.maps.LocationSource
 import com.google.android.libraries.maps.MapView
 import com.google.android.libraries.maps.model.LatLng
 import com.google.android.libraries.maps.model.MarkerOptions
@@ -21,27 +24,44 @@ import kotlinx.coroutines.launch
 @Composable
 fun ShowMapView(mainViewModel: MainViewModel, mapView: MapView) {
 
-        Column(
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth()
-                .background(Color.White)
-        ) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
 
-            AndroidView({ mapView }) { mapView ->
-                CoroutineScope(Dispatchers.Main).launch {
-                    val map = mapView.awaitMap()
-                    map.uiSettings.isZoomControlsEnabled = true
-                    for(item in mainViewModel.feed.value!!) {
-                        val marker =MarkerOptions()
-                        marker.title(item.name + " " + item.departureTime)
-                        marker.position(LatLng(item.latitude,item.longitude))
-                        map.addMarker(marker)
-                        
+        AndroidView({ mapView }) { mapView ->
+            CoroutineScope(Dispatchers.Main).launch {
+                val map = mapView.awaitMap()
+                var firstLocation= false
+                var zoomLevel = 9
+                val zoom = CameraUpdateFactory.zoomTo(zoomLevel.toFloat())
+                map.moveCamera(zoom)
+                map.uiSettings.isZoomControlsEnabled = true
+
+                for(item in mainViewModel.feed.value!!) {
+                    val marker =MarkerOptions()
+                    val position = LatLng(item.latitude,item.longitude)
+                    val cameraUpdate = CameraUpdateFactory.newLatLng(position)
+
+                    if(!firstLocation){
+                        firstLocation=true
+                        map.moveCamera(cameraUpdate)
 
                     }
-                        }}
-                }}
+                    marker.title(item.name + " " + if(item.typeId==0)"(Train)" else "(Tram)" )
+                    marker.snippet(item.departureTime)
+                    marker.position(position)
+                    map.addMarker(marker)
+
+
+
+                }
+
+
+            }}
+    }}
 
 
 
